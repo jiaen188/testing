33667f063215bd0c78c34943aa0cf05e42da6fac
jiaen188 <244189981@qq.com>
2018-07-07 21:03:13 +0800

    测试覆盖率

--- test/mocha.js
+++ test/mocha.js
@@ -1,5 +1,5 @@
 const {should, expect, assert} = require('chai')
-const {add, mul} = require('../src/math')
+const {add, mul, cover} = require('../src/math')
 
 describe('#math', () => {
   describe('add', () => {
@@ -16,6 +16,20 @@ describe('#math', () => {
   describe('mul', () => {
     it('should return 5 when 2 * 3', () => {
       expect(mul(2, 3), 6)
+    })
+  })
+
+  describe('cover', () => {
+    it('should return 1 when cover(2, 1)', () => {
+      expect(cover(2, 1)).to.equal(1)
+    })
+
+    it('should return 3 when cover(1, 2)', () => {
+      expect(cover(1, 2)).to.equal(3)
+    })
+
+    it('should return 4 when cover(2, 2)', () => {
+      expect(cover(2, 2)).to.equal(4)
     })
   })
 })
